20a2011918021ebe88df4eaac95abd9448ac0a81
AzureAD\alsaddigibrahim <alsaddig_ibrahim@stu.indianhills.edu>
2019-12-19 14:25:18 -0800

    Added Assignment 1.5 files

---
+++ week1/ibrahim-hello-world/src/app/shipping/shipping.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  template: `
+    <p>
+      Welcome from the ShippingComponent
+    </p>
+  `,
+  styles: []
+})
+export class ShippingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ week1/ibrahim-hello-world/src/app/shipping/shipping.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShippingComponent } from './shipping.component';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ShippingComponent],
+  exports: [ShippingComponent]
+})
+export class ShippingModule { }
