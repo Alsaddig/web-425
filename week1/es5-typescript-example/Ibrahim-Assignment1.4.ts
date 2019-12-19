9d6115d0755d90e0bb5a5ea93e12f2b41fb2700b
AzureAD\alsaddigibrahim <alsaddig_ibrahim@stu.indianhills.edu>
2019-12-19 12:41:47 -0800

    Added Assigment 1.4 file

---
+++ week1/es5-typescript-example/Ibrahim-Assignment1.4.ts
@@ -0,0 +1,6 @@
+var studentID = '12225';
+var studentName = 'Alsaddig Ibrahim';
+function myInfo(studentID, studentName) {
+    return 'My Student ID ' + studentID + ' \nMy Name ' + studentName;
+}
+console.log(myInfo(studentID, studentName));
